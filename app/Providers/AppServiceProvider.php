f3248f98eb1a8e61a2f1d243839bb60060c50e31
Giovanni Arriagada <garriagada@ecomac.cl>
2019-04-09 18:21:02 -0400

    se incorpora migracion y modelo de producto

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,12 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Schema;
+
 class AppServiceProvider extends ServiceProvider
+
 {
+    
     /**
      * Register any application services.
      *
@@ -23,6 +27,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
